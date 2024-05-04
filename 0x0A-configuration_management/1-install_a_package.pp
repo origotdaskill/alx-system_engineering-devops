46743b1db3f6bd25b1ed8909d58b715d4d10dc83
origotdaskill <mr.walid.ori@gmaill.com>
2024-05-04 19:18:36 +0100

    ori commit 🚀

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
-# using pupet, install flask
-
+# Ensure flask package is installed with specific version
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+}
+
+# Ensure Werkzeug package is installed with specific version
+package { 'werkzeug':
+  ensure   => '2.0.2',
+  provider => 'pip3',
 }
