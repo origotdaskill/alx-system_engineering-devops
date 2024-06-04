2aef5faedf545f55a10cc5d5becb2087a18ff7ef
origotdaskill <mr.walid.ori@gmaill.com>
2024-06-04 17:23:59 +0100

    ori commit

---
+++ 0x16-api_advanced/tests/2-main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+2-main
+"""
+import sys
+sys.path.insert(0, './')
+
+if __name__ == '__main__':
+    recurse = __import__('2-recurse').recurse
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        result = recurse(sys.argv[1])
+        if result is not None:
+            print(len(result))
+        else:
+            print("None")
