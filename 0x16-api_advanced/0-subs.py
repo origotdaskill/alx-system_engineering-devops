95f2cad598823b768a5676b39baaed341657e98c
origotdaskill <mr.walid.ori@gmaill.com>
2024-06-27 15:51:14 +0100

    ori commit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,35 +1,18 @@
 #!/usr/bin/python3
-"""
-Function that queries the Reddit API and returns the number of subscribers
-(not active users, total subscribers) for a given subreddit.
-If an invalid subreddit is given, the function should return 0
-"""
+"""function that queries the Reddit API and returns the number of subscribers
+for a given subreddit"""
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Function that queries the Reddit API
-    - If not a valid subreddit, return 0.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "Custom"}
-    
-    try:
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()  # Raise an error for bad status codes
-        
-        if response.status_code == 200:
-            return response.json().get("data").get("subscribers", 0)
-        else:
-            return 0
-    except requests.exceptions.HTTPError as http_err:
-        if response.status_code == 404:
-            print(f"Subreddit '{subreddit}' not found.")
-        else:
-            print(f"HTTP error occurred: {http_err}")
-        return 0
-    except requests.exceptions.RequestException as err:
-        print(f"Request error occurred: {err}")
+    """function that queries the Reddit API and returns the number of
+    subscribers for a given subreddit"""
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data.get('data').get('subscribers')
+    else:
         return 0
