83ce42bdebcde69d00a9672a8152973c7a3109ea
origotdaskill <mr.walid.ori@gmaill.com>
2024-06-27 15:57:46 +0100

    ori commit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python3
 """
-Function that queries the Reddit API and returns the number of subscribers
-(not active users, total subscribers) for a given subreddit.
-If an invalid subreddit is given, the function should return 0
+The module documentation
 """
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Function that queries the Reddit API
-    - If not a valid subreddit, return 0.
-    """
-    req = requests.get(
-        "https://www.reddit.com/r/{}/about.json".format(subreddit),
-        headers={"User-Agent": "Custom"},
-    )
+    """query a subreddit and retrive no of subscribers"""
 
-    if req.status_code == 200:
-        return req.json().get("data").get("subscribers")
+    headers = {"User-Agent": "Edson/1.0"}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, allow_redirects=False, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
     else:
-        return 0
+        return (0)
